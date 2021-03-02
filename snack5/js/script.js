42287703cbd2d2f470c16d84379574fabf40cbad
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-02 12:50:50 +0100

    controllo e stampa numeri dispari inseriti da utente

---
+++ snack5/js/script.js
@@ -0,0 +1,13 @@
+/* Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
+un numero, se è dispari inseriscilo nell’array. */
+// 1. inizializzo la variabile numero e l'array e chiedo all'utente di inserire 6 numeri
+var numero;
+var listaNumeri = [];
+for ( var i = 0; i < 6; i++){
+  numero = parseInt(prompt("Inserisci un numero"));
+  if (numero % 2 != 0) {
+    listaNumeri.push(numero);
+  }
+}
+// 2. stampo l'array con i numeri dispari
+console.log(listaNumeri);
