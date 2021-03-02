e57ba3fe1d44ab6cec4dc1d7f16b519e8913e669
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-02 12:24:10 +0100

    Lista invitati festa

---
+++ snack4/js/script.js
@@ -0,0 +1,20 @@
+/* In un array sono contenuti i nomi degli invitati alla festa del
+grande Inzaghi, chiedi all’utente il suo nome e comunicagli se
+può partecipare o no alla festa. */
+// 1. inizializzo array invitati
+var invitati = ["Samuele", "Andrea", "Giuseppe", "Tommaso", "Francesca", "Lorena", "Gianluca", "Gianluigi", "Caterina", "Alessio", "Clelia", "Alessandro", "Matia", "Gabriele"];
+// 2. chiedo all'utente il suo nome e inizializzo variabile per controllo
+var nome = prompt("Inserisci il tuo nome");
+var presente = false;
+// 3. controllo se il nome è in lista
+for(var i = 0; i < invitati.length; i++){
+  if (nome == invitati[i]){
+    presente = true;
+  }
+}
+// 4. do il permesso all'utente di entrare
+if (presente == true) {
+  console.log("Puoi entrare alla festa");
+} else{
+  console.log("Non puoi entrare alla festa")
+}
