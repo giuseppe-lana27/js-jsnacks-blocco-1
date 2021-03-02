516f332c74b33a96dd0b2794075fbd58b7003791
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-02 12:14:27 +0100

    modifica condizione ciclo

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -3,9 +3,9 @@ un numero. Il programma stampa la somma di tutti i numeri
 inseriti.*/
 // 1. chiedo all'utente di inserire dieci numeri
 var somma = 0;
-for (var i = 0; i <= 10; i++){
+for (var i = 0; i < 10; i++){
   var numero = parseInt(prompt("Inserisci un numero"));
   somma = somma + numero;
 }
-//stampo la somma dei dieci numeri
+// 2. stampo la somma dei dieci numeri
 console.log( "La somma dei numeri inseriti è: " + somma);
