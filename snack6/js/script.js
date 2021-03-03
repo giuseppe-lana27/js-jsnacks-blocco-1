f3560210699fa045b8a7c9ef782bd828a2636ffd
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-03 12:01:21 +0100

    cubo n numeri inseriti da utente

---
+++ snack6/js/script.js
@@ -0,0 +1,17 @@
+/* Stampa il cubo dei primi N numeri, dove N è un numero
+indicato dall’utente.*/
+// 1. inizializzo un array per numeri da elevare al cubo
+var numeri = [];
+// 2. chiedo all'utente di inserire un numero e lo inserisco nell'array
+var numeroUtente = parseInt(prompt("Inserisci un numero"));
+if (isNaN(numeroUtente)){
+  numeroUtente = parseInt(prompt("Inserisci un numero in cifre"));
+}
+numeri.push(numeroUtente);
+console.log(numeri);
+// 3. ciclo per elevamento al cubo e inizializzo variabile potenza
+var potenza;
+for (var i = 1; i < numeroUtente; i++){
+  potenza = Math.pow(i, 3);
+  console.log(potenza);
+}
